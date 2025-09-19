c144f2442ab695d0b89f19558131a76e2c10b8bc
rishi <potlacheruvurishi@gmail.com>
2025-09-19 17:08:13 +0530

    feat: Add login module

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,20 +2,26 @@ const router = require('express').Router();
 const User = require('../models/user.model');
 
 router.route('/signup').post(async (req, res) => {
-    const { username, password } = req.body;
-
-    if (!username || !password) {
-        return res.status(400).json({ error: 'Username and password are required' });
-    }
-
-    const newUser = new User({ username, password });
-
-    try {
-        await newUser.save();
-        res.status(201).json({ message: 'User created' });
-    } catch (err) {
-        res.status(400).json({ error: 'Username already exists. ' + err.message });
-    }
+  const { username, password } = req.body;
+  const newUser = new User({ username, password });
+  try {
+    await newUser.save();
+    res.status(201).json('User created!');
+  } catch (err) {
+    res.status(400).json('Error: ' + err);
+  }
 });
 
+router.route('/login').post(async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(400).json('Invalid credentials.');
+  }
+  if (user.password !== password) {
+    return res.status(400).json('Invalid credentials.');
+  }
+  res.status(200).json('Login successful!');
+});
 module.exports = router;
