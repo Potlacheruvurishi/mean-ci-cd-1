1dc50452678201384199ca285a6212210f9740dc
rishi <potlacheruvurishi@gmail.com>
2025-09-19 15:27:32 +0530

    router oulet updated

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterModule],
+  imports: [RouterModule, RouterOutlet], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
